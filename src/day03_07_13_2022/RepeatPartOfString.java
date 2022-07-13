f94817915f75a4ea4c954b71a6118b6e1e22abe1
AdamB25 <barisaltunit@gmail.com>
2022-07-13 17:10:30 -0400

    Day 3 String Methods are done

--- src/day03_07_13_2022/RepeatPartOfString.java
+++ src/day03_07_13_2022/RepeatPartOfString.java
@@ -13,37 +13,32 @@ public class RepeatPartOfString {
 				---------------------------------------
 				(substring-length-isEmpty)
          */
-                   //01234
-        String word="Hello";
+        //01234
+        String word = "Hello";
 
         //if you want ot get one character you can use charAt()
-        char l=word.charAt(2);
+        char l = word.charAt(2);
 
         //if you want to get length of string length()
 
         int length = word.length();
-if (length<2)
-    System.out.println(word +" length can not be smaller than 2");
+        if (length < 2)
+            System.out.println(word + " length can not be smaller than 2");
 
-        char lastCh=word.charAt(length-1);
-        char secondLastCh=word.charAt(length-2);
+        char lastCh = word.charAt(length - 1);
+        char secondLastCh = word.charAt(length - 2);
 
-                             //01234
-                             //Hello
+        //01234
+        //Hello
         //if you want to get some part of your string you can use substring()
-        String el=word.substring(1,3); //el
-        String lo=word.substring(3,5); //lo
-        String lo2=word.substring(3); //lo   //length-2
-        String lo3=word.substring(length-2);
+        String el = word.substring(1, 3); //el
+        String lo = word.substring(3, 5); //lo
+        String lo2 = word.substring(3); //lo   //length-2
+        String lo3 = word.substring(length - 2);
 
 
-        System.out.println(lo3+lo3+lo3);
+        System.out.println(lo3 + lo3 + lo3);
         System.out.println(lo3.repeat(3));
-
-
-
-
-
 
 
     }
