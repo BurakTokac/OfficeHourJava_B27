237c0d00965ed4461abbcc687879c27e87d56cfd
AdamB25 <barisaltunit@gmail.com>
2022-08-03 16:20:00 -0400

    Day 6 PrimeNumber task is done

--- src/day06_08_03_2022/MyCalculatorTest.java
+++ src/day06_08_03_2022/MyCalculatorTest.java
@@ -5,5 +5,8 @@ public class MyCalculatorTest {
         //if you want to call one method from different class
         //we need to use ClassName.MethodName
         System.out.println("MyCalculator.add(5, 4) = " + MyCalculator.add(5, 4));
+
+        String name="Adam";
+        System.out.println(name);
     }
 }
