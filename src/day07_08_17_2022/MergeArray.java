a4f15a587a0ac9de415774fecc00dfe8cf23c744
AdamB25 <barisaltunit@gmail.com>
2022-08-17 15:05:21 -0400

    Day 7 MergeArray is done

--- src/day07_08_17_2022/MergeArray.java
+++ src/day07_08_17_2022/MergeArray.java
@@ -9,13 +9,36 @@ public class MergeArray {
         int[] b={4,5,6};
         int[] c={7,8,9};
         int[] d={10,11,12};
-     int[] merged=   mergeTwoArray(a,b);
+     int[] merged=   mergeArray(a,b);
         System.out.println("Arrays.toString(merged) = " + Arrays.toString(merged));
+    int[] merged2=    mergeArray(a,b,c);
 
     }
 
-    public static int[] mergeTwoArray(int[] a, int[] b) {
+    public static int[] mergeArray(int[] array1, int[] array2) {
        //I need to create one dummy array
+        int [] dummy=new int[array1.length+array2.length];
+
+        // I need to put my first array into dummy
+        int index=0;
+        for (int each:array1) {
+            dummy[index++]=each;
+        }
+        //int [] dummy={1,2,3}
+
+        //I need to put my second array into dummy
+
+        for (int each:array2) {
+            dummy[index++]=each;
+        }
+        //int [] dummy={1,2,3,4,5,6}
+        //I need to return dummy
+
+        return dummy;
+    }
+/*
+    public static int[] mergeArray(int[] a, int[] b,int [] c) {
+        //I need to create one dummy array
         int [] dummy=new int[a.length+b.length];
 
         // I need to put my first array into dummy
@@ -32,9 +55,39 @@ public class MergeArray {
         }
         //int [] dummy={1,2,3,4,5,6}
         //I need to return dummy
+        //I need to put my third array into dummy
+
+        for (int each:c) {
+            dummy[index++]=each;
+        }
+
 
         return dummy;
     }
+
+ */
+/*
+    public static int[] mergeArray(int[] a, int[] b,int [] c) {
+        int[] mergedTwoArray = mergeArray(a, b);//{1,2,3,4,5,6}
+
+        int[] mergedThreeArray = mergeArray(mergedTwoArray, c);
+
+        return mergedThreeArray;
+
+    }
+
+
+
+ */
+
+    public static int[] mergeArray(int[] a, int[] b,int [] c) {
+
+      return   mergeArray(mergeArray(a, b),c);
+
+
+    }
+
+
 
 
 }
