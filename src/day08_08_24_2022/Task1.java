28157bc6704fecc752688acd1ebf3c82014a3595
AdamB25 <barisaltunit@gmail.com>
2022-08-24 15:23:51 -0400

    Day 8 added Task1.java file

---
+++ src/day08_08_24_2022/Task1.java
@@ -0,0 +1,22 @@
+package day08_08_24_2022;
+
+public class Task1 {
+    public static void main(String[] args) {
+        /*
+        Task 1 : Four or less
+
+
+					Given an ArrayList of Strings, go through and read only Strings that are 4 characters or less.
+
+					Take those Strings and put them into a different ArrayList
+
+					Ex:
+
+					Input:
+					“apples”, “tree”, “loop, “cat”, “animal”, “shortcut”
+
+					Output:
+					[ tree, loop, cat ]
+         */
+    }
+}
