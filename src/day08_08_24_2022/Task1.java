ed92e6261d5056f4698b5ab3ac170cb421d13271
AdamB25 <barisaltunit@gmail.com>
2022-08-24 17:11:58 -0400

    Day 8 added Task1.java file

--- src/day08_08_24_2022/Task1.java
+++ src/day08_08_24_2022/Task1.java
@@ -1,5 +1,8 @@
 package day08_08_24_2022;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Task1 {
     public static void main(String[] args) {
         /*
@@ -13,10 +16,39 @@ public class Task1 {
 					Ex:
 
 					Input:
-					“apples”, “tree”, “loop, “cat”, “animal”, “shortcut”
+					"apples", "tree", "loop, "cat", "animal", "shortcut"
 
 					Output:
 					[ tree, loop, cat ]
          */
+
+        ArrayList<String> list=new ArrayList<>(Arrays.asList("apples", "tree", "loop", "cat", "animal", "shortcut"));
+
+        //get the words one by one
+        ArrayList<String> empty = new ArrayList<>();
+        for (String s : list) {
+            //check words if its length smaller than or equal 4
+          boolean result=  checkWordLength(s);
+            //put the word another empty list if it is true
+
+            if (result){
+                empty.add(s);
+            }
+
+        }
+        System.out.println("empty = " + empty);
+    }
+
+    private static boolean checkWordLength(String s) {
+      /*  if (s.length()<=4) {
+            return true;
+        }
+        else {
+            return false;
+        }
+
+       */
+    return  s.length()<=4;
+
     }
 }
